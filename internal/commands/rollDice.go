c1c55c03a90fcb0a7bc5279cb65fa239d7bcf2b1
c-bec-k <cruthenbeck@gmail.com>
2024-07-22 07:40:13 -0400

    fixed display issue of emoji

--- internal/commands/rollDice.go
+++ internal/commands/rollDice.go
@@ -12,7 +12,7 @@ type die struct {
 
 var dice = []die{
 	{result: -1, emoji: "<:dFm:1264228207536640073>"},
-	{result: 0, emoji: "<:dF0:1264228218588626979>"},
+	{result: 0, emoji: "<:dF0:1264228188775780434>"},
 	{result: 1, emoji: "<:dFp:1264228218588626979>"},
 }
 
